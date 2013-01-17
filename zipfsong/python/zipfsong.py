94263637afe6d1f16b27b498102b6a3128dc52db
Marcus Ortiz <mportiz08@gmail.com>
2013-01-16 19:53:25 -0800

    Hopefully this is more efficient.

--- zipfsong/python/zipfsong.py
+++ zipfsong/python/zipfsong.py
@@ -8,7 +8,7 @@ This is my solution to Spotify's "Zipf's song" Tech Puzzle.
 """
 
 import sys
-from functools import cmp_to_key
+import heapq
 
 class Song(object):
   def __init__(self, listens, name, track):
@@ -17,49 +17,27 @@ class Song(object):
   def quality(self):
     return self.listens * self.track
 
-class SongSelector(object):
-  def __init__(self, num_songs, num_selections, songs):
-    self._num_songs, self._num_selections = num_songs, num_selections
-    self._songs = songs
-  
-  def select(self):
-    sorted_songs = sorted(self._songs, key=cmp_to_key(compare_songs))
-    
-    return sorted_songs[:self._num_selections]
-
-def compare_songs(a, b):
-  if a.quality() < b.quality():
-    return 1
-  elif a.quality() > b.quality():
-    return -1
-  else:
-    if a.track < b.track:
-      return -1
-    elif a.track > b.track:
-      return 1
-    else:
-      return 0
-
 def parse_songs():
-  first_line = sys.stdin.readline()
-  rest_lines = sys.stdin.readlines()
-  
+  first_line     = sys.stdin.readline()
   num_songs      = int(first_line.split()[0])
   num_selections = int(first_line.split()[1])
   
   songs = []
-  for i, line in enumerate(rest_lines):
-    listens = int(line.split()[0])
-    name    = line.split()[1]
-    songs.append(Song(listens, name, i + 1))
+  for i in range(num_songs):
+    next_line = sys.stdin.readline()
+    
+    listens = int(next_line.split()[0])
+    name    = next_line.split()[1]
+    song    = Song(listens, name, i + 1)
+    heapq.heappush(songs, (-song.quality(), song))
   
-  return num_songs, num_selections, songs
+  return num_selections, songs
 
 def main(argv=None):
-  num_songs, num_selections, songs = parse_songs()
-  selection = SongSelector(num_songs, num_selections, songs).select()
-  for song in selection:
-    print song.name
+  num_selections, songs = parse_songs()
+  for i in range(num_selections):
+    song = heapq.heappop(songs)[1]
+    print(song.name)
   
   return 0
 
