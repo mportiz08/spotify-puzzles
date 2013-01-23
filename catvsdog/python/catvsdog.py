4b93602a41a3a780bff067e16eeb3291032cb416
Marcus Ortiz <mportiz08@gmail.com>
2013-01-23 15:53:46 -0800

    Starting catvsdog puzzle.

---
+++ catvsdog/python/catvsdog.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+catvsdog
+~~~~~~~~~~~~~
+
+This is my solution to Spotify's "Cat vs. Dog" Tech Puzzle.
+"""
+
+import sys
+
+def main(argv=None):
+  # TODO
+  
+  return 0
+
+if __name__ == '__main__':
+  status = main()
+  sys.exit(status)
